76b03b8dc41a86eb312d7a16c7bf7ed1b7fb9a99
Amirali <57327052+itsamirali@users.noreply.github.com>
2022-01-20 19:19:14 +0330

    create module css for home page and sidebar

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,86 +5,36 @@ import GithubIcon from '@material-ui/icons/GitHub';
 import YoutubeIcon from '@material-ui/icons/YouTube';
 import Particle from '../Components/Particle';
 
+import cx from "classnames"
+
+import styles from "./Home.module.css"
+
 function HomePage() {
     return (
-        <HomePageStyled>
+        <div className={cx(styles.homeStyles)}>
             <div className="particle-con">
                 <Particle />
             </div>
-            <div className="typography">
-                <h1>Hi, I'm <span>Lorem Ipsum</span></h1>
+            <div className={cx(styles.typography)}>
+                <h1>Hi, I'm <span>Amirali Karimi</span></h1>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Atque nihil voluptates ea dolore vel 
-                    repellat? Quia tenetur non quam exercitationem. Lorem ipsum dolor sit amet.
+                Frontend Web Developer | efficient web development services
                 </p>
-                <div className="icons">
-                    <a href="https://codepen.io/pen/" className="icon i-facebook">
+                <div className={cx(styles.icons)}>
+                    <a href="https://codepen.io/pen/" className={cx(styles.icon, "i-facebook")}>
                         <FacebookIcon />
                     </a>
-                    <a href="https://codepen.io/pen/" className="icon i-github">
+                    <a href="https://codepen.io/pen/" className={cx(styles.icon, "i-github")}>
                         <GithubIcon />
                     </a>
-                    <a href="https://codepen.io/pen/" className="icon i-youtube">
+                    <a href="https://codepen.io/pen/" className={cx(styles.icon, "i-youtube")}>
                         <YoutubeIcon />
                     </a>
                 </div>
             </div>
-        </HomePageStyled>
+        </div>
     )
 }
 
-const HomePageStyled = styled.header`
-    width: 100%;
-    height: 100vh;
-    position: relative;
-   
-    .typography{
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        text-align: center;
-        width: 80%;
-
-        .icons{
-            display: flex;
-            justify-content: center;
-            margin-top: 1rem;
-            .icon{
-                border: 2px solid var(--border-color);
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                border-radius: 50%;
-                transition: all .4s ease-in-out;
-                cursor: pointer;
-                &:hover{
-                    border: 2px solid var(--primary-color);
-                    color: var(--primary-color);
-                }
-                &:not(:last-child){
-                    margin-right: 1rem;
-                }
-                svg{
-                    margin: .5rem;
-                }
-            }
-
-            .i-youtube{
-                &:hover{
-                    border: 2px solid red;
-                    color: red;
-                }
-            }
-            .i-github{
-                &:hover{
-                    border: 2px solid #5F4687;
-                    color: #5F4687;
-                }
-            }
-        }
-    }
-`;
 
 export default HomePage;
